181f92e1efb7c37231593da2d8c9f76664fd1243
akobusinska <aleksandra@ffm-online.com>
2019-09-27 14:05:52 +0200

    Added unit tests

--- src/test/java/assigment/products/AssigmentProductsApplicationTests.java
+++ src/test/java/assigment/products/AssigmentProductsApplicationTests.java
@@ -3,21 +3,17 @@ package assigment.products;
 import static org.junit.Assert.assertTrue;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import assigment.products.repository.ProductRepository;
 import assigment.products.service.ProductService;
-import lombok.extern.slf4j.Slf4j;
 
 @RunWith(SpringRunner.class)
 @Transactional
